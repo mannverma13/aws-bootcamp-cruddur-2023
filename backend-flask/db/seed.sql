f8f918f25e3720f3e782c53401c2dbada96743a9
mannverma13 <mvmanverma@gmail.com>
2023-03-27 19:00:26 +0000

    integrate dyanmo db with the application

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+  ('Andrew Brown','andrew@exampro.co' , 'mannverma' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'manu' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'mannverma' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
