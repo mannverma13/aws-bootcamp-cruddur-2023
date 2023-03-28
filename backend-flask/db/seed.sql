6ff7dd665acd6ce4deca0e7978dda7bfee3304e9
mannverma13 <mvmanverma@gmail.com>
2023-03-28 16:45:06 +0000

    updated user details in seed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'mannverma' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'manu' ,'MOCK');
+  ('Manish Verma','mvmanverma@gmail.com' , 'mannverma' ,'MOCK'),
+  ('Manu sharma','manishverma978@gmail.com' , 'manu' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
