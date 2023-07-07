4bc1d75497385a16f3826d722896eeeae5b9992c
mannverma13 <mvmanverma@gmail.com>
2023-07-07 11:46:27 +0000

    fixed rollbar issue

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -52,6 +52,7 @@ import os
 import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
+from time import strftime
 
 
 # Configuring Logger to Use CloudWatch
@@ -80,11 +81,9 @@ FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
 #Rollbar----
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
+def init_rollbar(app):
+  rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+  rollbar.init(
         # access token
         rollbar_access_token,
         # environment name
@@ -94,8 +93,8 @@ def init_rollbar():
         # flask already sets up logging
         allow_logging_basic_config=False)
 
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+  # send exceptions from `app` to rollbar, using flask's signal system.
+  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
